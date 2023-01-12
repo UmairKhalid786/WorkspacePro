8273262723aacf4edfc674f1c8a398311f912d27
Umair Khalid <Umair.khalid786@outlook.com>
2023-01-12 01:50:06 +0100

    Dynamic version code added in about me dialog

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "WorkSpacePro"
-            packageVersion = "1.0.0"
+            packageVersion = "1.0.1"
         }
     }
 }

005ce08b2866a9bcedc4832098fc58172865ab10
Umair Khalid <Umair.khalid786@outlook.com>
2023-01-18 00:55:45 +0100

    #17931432 Fake Signin with one input box added

--- src/jvmMain/kotlin/screens/signin/SigninScreen.kt
+++ src/jvmMain/kotlin/screens/signin/SigninScreen.kt
@@ -1,13 +1,14 @@
 package screens.signin
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,10 +18,14 @@ fun SigninScreen(
     onGotoHomeClick: () -> Unit,
     onGotoSignupClick: () -> Unit,
 ) {
-    var scale = animateFloatAsState(0.5f)
+
+    var email by remember { mutableStateOf("") }
+
+    var loaded by remember { mutableStateOf(false) }
+    val scale = animateFloatAsState(if (loaded)  0.8f else 1f)
 
     LaunchedEffect(Unit){
-
+        loaded = true
     }
 
     Column(
@@ -31,20 +36,21 @@ fun SigninScreen(
         Image(
             painter = painterResource("icons/web/icon.png"),
             contentDescription = "Logo",
-            modifier = Modifier.size(150.dp)
+            modifier = Modifier.size(200.dp).scale(scale.value)
         )
         Spacer(modifier = Modifier.size(50.dp))
         Text("Welcome to WorkspacePro", style = MaterialTheme.typography.h4)
         Spacer(modifier = Modifier.size(25.dp))
-        TextField("", onValueChange = {
 
+        TextField(email, onValueChange = {
+            email = it
         })
 
         Spacer(modifier = Modifier.size(50.dp))
 
         // Go back button
-        Button(onClick = onGotoHomeClick) {
-            Text(text = "Home")
+        Button(onClick = onGotoHomeClick, enabled = email.isNotBlank()) {
+            Text(text = "Login")
         }
         // Go back button
         TextButton(onClick = onGotoSignupClick) {
